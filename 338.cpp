6529a575e095a42f6158783de0c930d04eda90f1
Potato Chip <thepotatoxchip@gmail.com>
2021-11-12 11:26:15 +0530

    :sparkles: solve 338

---
+++ 338.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/counting-bits/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> countBits(int n) {
+    vector<int> ans(n + 1);
+    ans[0] = 0;
+
+    for (int i = 1; i <= n; ++i) {
+      ans[i] = ans[i / 2] + i % 2;
+    }
+
+    return ans;
+  }
+};
